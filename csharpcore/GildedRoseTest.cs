372f308bdb853211772d991e950940a0c3b1950a
Cory Koehler <cjiek9181@gmail.com>
2020-06-15 18:01:28 -0400

    Initial commit for porting into github

---
+++ csharpcore/GildedRoseTest.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using System.Collections.Generic;
+
+namespace csharpcore
+{
+    public class GildedRoseTest
+    {
+        [Fact]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal("foo", Items[0].Name);
+        }
+
+        [Fact]
+        public void ConjuredManaCakeShouldLoseQualityTwiceAsFastAndSellinStaysAtNormalRate ()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.Equal("Conjured Mana Cake", Items[0].Name);
+            Assert.Equal(2, Items[0].SellIn);
+            Assert.Equal(4, Items[0].Quality);
+        }
+    }
+}
